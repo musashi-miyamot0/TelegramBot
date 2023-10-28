d2366a32c411c590ee009a48967cd17cf49ffa86
Murad_Sahsinov <swiftkey382@gmail.com>
2023-10-28 23:28:31 +0300

    First connit

--- modules/screenshot_pc.py
+++ modules/screenshot_pc.py
@@ -6,7 +6,6 @@ import pyautogui
 from aiogram.types import FSInputFile
 
 
-
 class ScreenshotPC:
     '''    Класс `ScreenshotPC` предоставляет функционал для создания снимков экрана
             и отправки их через Telegram-бота.
@@ -27,12 +26,12 @@ class ScreenshotPC:
         '''Делает скриншот экран инструментом scrot. ONLY linux
             Принимает message'''
         await message.delete()
-            keyboard = getattr(self.control_variable, 'tracking_logic_keyboard')
-            terminal = getattr(self.control_variable, 'tracking_logic_terminal')
-       
-            
+        keyboard = getattr(self.control_variable, 'tracking_logic_keyboard')
+        terminal = getattr(self.control_variable, 'tracking_logic_terminal')
+
         if not (keyboard and terminal):
-            subprocess.call(['scrot', '/home/deogen/pythonProject2/screen.png'])
+            subprocess.call(
+                ['scrot', '/home/deogen/pythonProject2/screen.png'])
             file = FSInputFile('/home/deogen/pythonProject2/screen.png')
             await message.answer_photo(file)
             os.remove('/home//deogen//pythonProject2/screen.png')
@@ -40,6 +39,8 @@ class ScreenshotPC:
     async def scren_windows(self, message):
         ph = pyautogui.screenshot()
         ph.save("screen.png")
-        scren = FSInputFile("C:\\Users\\murad\\pythonProject2\\screen.png")
+        current_path = os.getcwd()
+        file = os.path.join(current_path, 'screen.png')
+        scren = FSInputFile(file)
         await message.answer_photo(scren)
-        os.remove("C:\\Users\\murad\\pythonProject2\\screen.png")
+        os.remove(file)
